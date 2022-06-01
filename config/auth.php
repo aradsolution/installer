7a645595cafcd3dd16ee8a3ec674d8d3cc891c85
Vahid Farid <farid@aradsolution.com>
2022-06-01 13:15:46 +0300

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => Arad\Core\Models\User::class,
         ],
 
         // 'users' => [
