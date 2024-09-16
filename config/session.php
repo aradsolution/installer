c433c98ec69b0beb3a5971e59439980367a03123
Vahid Farid <farid@aradsolution.com>
2024-09-16 12:06:30 +0330

    Revert "Update session.php"
    This reverts commit 1a73605b5553431c1911e02eb431c8aeb63e0918.

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120) + 30,
+    'lifetime' => env('SESSION_LIFETIME', 120),
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', true),
 
