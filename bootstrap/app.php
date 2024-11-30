0d9127225d9ec2d94b2dc376a4f08c5ec81aad7b
Vahid Farid <farid@aradsolution.com>
2024-11-30 14:55:13 +0330

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->remove([
+            PreventRequestsDuringMaintenance::class,
+        ]);    
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
