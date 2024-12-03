74e969436220328571e3cd316f7ad44edf0d62ca
Vahid Farid <farid@aradsolution.com>
2024-12-03 16:12:09 +0330

    Revert "Update app.php"
    This reverts commit 0d9127225d9ec2d94b2dc376a4f08c5ec81aad7b.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->remove([
-            PreventRequestsDuringMaintenance::class,
-        ]);    
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
